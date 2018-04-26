cc525ed99511329dec9dc629d080f9434e7f37e3
Jacob Shirley <jakeshirley2@gmail.com>
2018-04-26 19:18:33 +0100

    Implementing a python interpreter scenario. The various interfaces need wiring up and it's done.

---
+++ Assets/UI Scripts/LoadCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadCode : MonoBehaviour {
+
+    private CodeSnippets codeSnippets;
+
+    [Serializable]
+    private class Snippet
+    {
+        public string name;
+        public string code;
+    }
+
+    [Serializable]
+    private class CodeSnippets
+    {
+        public Snippet[] snippets;
+    }
+
+    // Use this for initialization
+    void Start () {
+        codeSnippets = JsonUtility.FromJson<CodeSnippets>(File.ReadAllText("Assets/JSON/python-snippets.json"));
+        Debug.Log(codeSnippets.snippets[0].code);
+    }
+
+    public void LoadCodeInto(InputField inputField)
+    {
+        inputField.text = codeSnippets.snippets[0].code;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+}
+
+
