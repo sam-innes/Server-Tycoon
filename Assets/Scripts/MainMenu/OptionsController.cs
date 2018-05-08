1594210e4eceebddf3929bebbd58b32d1f219478
Luke <lmmurray1234@gmail.com>
2018-05-08 18:44:45 +0100

    Main menu added

---
+++ Assets/Scripts/MainMenu/OptionsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour {
+
+    public GameObject main;
+    public GameObject options;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Return()
+    {
+        main.SetActive(true);
+        options.SetActive(false);
+    }
+}
