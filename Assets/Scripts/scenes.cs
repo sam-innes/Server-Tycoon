6b0cf37af69dc583154c5993a3af40518f3f2b75
Sam <sam.innes@me.com>
2018-05-08 16:28:56 +0100

    Sorted some files out, removed python parser (bugs), and added tweaks to movement script. Also added a scene manager.

--- Assets/Scripts/scenes.cs
+++ Assets/Scripts/scenes.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class scenes : MonoBehaviour {
-
 	void Start(){
-		Debug.Log("test");
-		Debug.Log(SceneManager.GetActiveScene());
+		DontDestroyOnLoad(this.gameObject);
+		SceneManager.SetActiveScene(SceneManager.GetSceneByName("game"));
 	}
+
+	public void loadEmail(){
+		SceneManager.LoadScene("EmailClient", LoadSceneMode.Additive);
+	}
+
+	public void returnMain(){
+		SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("EmailClient"));
+		Debug.Log("test");
+		GameObject.Find("manBlue_stand").GetComponent<playerController>().movementActivate();
+	}
+
 }
