1d06b313327313c69b893e030efa9b744e222d24
Jacob Shirley <jakeshirley2@gmail.com>
2018-05-10 11:54:45 +0100

    Server triggers now at a basic level.

--- Assets/UI Scripts/NewServerPlacement.cs
+++ Assets/UI Scripts/NewServerPlacement.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class NewServerPlacement : MonoBehaviour {
-    private Object serverPrefab;
+    public Object hoverServerPrefab;
+    public Object serverPrefab;
     private GameObject hoverUI; 
 
     void Awake()
     {
-        serverPrefab = Resources.Load("Server Prefabs/ServerPrefab");
         CreateNew();
         SetActive(false);
     }
 
     public void CreateNew()
     {
-        hoverUI = (GameObject)Instantiate(serverPrefab);
+        hoverUI = (GameObject)Instantiate(hoverServerPrefab);
     }
 
     public void OnMouseOver()
@@ -32,16 +32,14 @@ public class NewServerPlacement : MonoBehaviour {
         if (hoverUI.activeSelf && hoverUI.GetComponent<ServerScript>().InValidPosition())
         {
             Place();
-            CreateNew();
             SetActive(false);
         }
     }
 
     public void Place()
     {
-        hoverUI.GetComponent<SpriteRenderer>().sortingOrder = 0;
-        Destroy(hoverUI.GetComponent<ServerScript>());
-        hoverUI.GetComponent<BoxCollider2D>().isTrigger = false;
+        GameObject newServer = (GameObject)Instantiate(serverPrefab);
+        newServer.transform.position = hoverUI.transform.position;
     }
 
     public void SetActive(bool state)
---
+++ Assets/UI Scripts/ServerPlacedScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerPlacedScript : MonoBehaviour {
+
+    public Text Prompt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Prompt.text = "Press 'e' to configure server";
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Prompt.text = "";
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
